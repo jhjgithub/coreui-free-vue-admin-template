e2cdd6594e193195877ecca6a99cccc989122492
Jiho Jung <irongate@naver.com>
2018-12-29 12:05:13 +0900

    add custom sidebar

---
+++ src/views/Sidebar/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="sidebar" v-on-clickaway="hideMobile">
+    <slot>Sidebar</slot>
+  </div>
+</template>
+<script>
+import { mixin as clickaway } from 'vue-clickaway'
+// import { hideMobile } from '../../mixins/hideMobile'
+import { hideMobile } from '@coreui/vue/src/mixins/hideMobile'
+
+export default {
+  name: 'sidebar',
+  mixins: [ clickaway, hideMobile ],
+  props: {
+    fixed: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted: function () {
+    this.isFixed()
+  },
+  methods: {
+    isFixed () {
+      this.fixed ? document.body.classList.add('sidebar-fixed') : document.body.classList.remove('sidebar-fixed')
+      return this.fixed
+    }
+  }
+}
+</script>
